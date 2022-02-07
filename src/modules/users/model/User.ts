72adbb391e3742908dce9ca56c8ccf2a328d1a65
Gabriel Martins Rennó <gabrielmrenno@gmail.com>
2022-02-07 19:43:47 -0300

    Finalizando projeto

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,21 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+
+  //  Passar a responsabilidade de criar um novo id para o modelo
+  //    - através do método contructor(), verifica se o id(this.id) não existir, criar um novo
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+      this.admin = false;
+    }
+  }
 }
 
 export { User };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("To list users, you must be a admin");
+    }
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
